763e9fd83183bc80d6987d2305907b6f62097e68
AymKh <aymen4real@gmail.com>
2022-06-30 13:54:57 +0100

    got rid of hero section

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './pages/header/header.component';
-import { HeroFilterComponent } from './pages/hero-filter/hero-filter.component';
 import { ProductsGridComponent } from './pages/products-grid/products-grid.component';
 import { ProductCardComponent } from './pages/products-grid/product-card/product-card.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -15,7 +14,6 @@ import { HomeComponent } from './pages/home/home.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroFilterComponent,
     ProductsGridComponent,
     ProductCardComponent,
     routingComponents,
