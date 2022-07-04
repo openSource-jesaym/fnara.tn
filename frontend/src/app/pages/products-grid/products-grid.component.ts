c95170f55b694d4724feb336326a1d11167d1c65
saifmogaadi12 <mogaadisaif7@gmail.com>
2022-07-04 17:49:20 +0100

    sidebar

--- frontend/src/app/pages/products-grid/products-grid.component.ts
+++ frontend/src/app/pages/products-grid/products-grid.component.ts
@@ -8,15 +8,238 @@ import { ProductsService } from '../../services/products.service';
   styleUrls: ['./products-grid.component.scss']
 })
 export class ProductsGridComponent implements OnInit {
-  productsList: any;
+  // productsList: any;
+  productsList: any = [
+    {
+      "_id": "62b9d9f68c38898f10bffaf1",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f68c38898f10bffaf3",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f68c38898f10bffaf5",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f68c38898f10bffaf7",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffaf9",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffafb",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffafd",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffaff",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+    {
+      "_id": "62b9d9f78c38898f10bffb01",
+      "sku": 2154872,
+      "name": "Jordan Sneakers",
+      "date_added": [
+        "2022-06-27T16:21:10.322Z"
+      ],
+      "price": 78.2,
+      "quantity": 152,
+      "description": "JUst some sneakers..",
+      "image": "https://www.freeiconspng.com/uploads/no-image-icon-23.jpg",
+      "__v": 0
+    },
+  ];
   loading= true;
   constructor(
     private ProductsService: ProductsService
   ) { }
 
   ngOnInit(): void {
-    this.loading = true;
-    this.getDB_Data();
+    this.loading = false;
+    // this.getDB_Data();
   }
 
   getDB_Data() {
--- frontend/src/app/pages/sidebar/sidebar.component.ts
+++ frontend/src/app/pages/sidebar/sidebar.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  categories: any[] = [
+    {
+      name: 'Cars',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Clothes',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Computers',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Boats',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Books',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Sports',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Gaming',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Movies',
+      link: 'https://www.google.com',
+    },
+    {
+      name: 'Music',
+      link: 'https://www.google.com',
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
