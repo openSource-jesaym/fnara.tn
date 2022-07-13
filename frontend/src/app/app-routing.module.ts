3921c443f1c0358dfe515ad3902a9b558256a919
aymkh <aymen4real@gmail.com>
2022-07-13 23:28:00 +0100

    About us initial template

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductManageComponent} from "./pages/product-manage/product-manage.component";
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/footer/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
   { path: 'manage-product', component: ProductManageComponent },
   { path: '**', redirectTo: 'home'},
 ];
