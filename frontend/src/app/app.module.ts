be12c12ade37663d7afc94338e10ac6974e53347
aymkh <aymen4real@gmail.com>
2022-08-15 18:13:36 +0100

    new header using Angular Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,25 +5,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './pages/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { ProductsGridComponent } from './pages/products-grid/products-grid.component';
-import { ProductCardComponent } from './pages/products-grid/product-card/product-card.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AboutComponent } from './pages/footer/about/about.component';
 import { ContactComponent } from './pages/footer/contact/contact.component';
 import { FaqComponent } from './pages/footer/faq/faq.component';
-import { ToggleMenuComponent } from './pages/header/toggle-menu/toggle-menu.component';
+import { ToggleMenuComponent } from './components/header/toggle-menu/toggle-menu.component';
 
 
 // ANGULAR MATERIAL
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { LogoComponent } from './shared/logo/logo.component';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -52,7 +53,8 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatMenuModule,
     MatDividerModule,
     MatExpansionModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
